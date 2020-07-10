74bdaca8e33f9814f180c4b38d47c6b4cee372c5
Sithum Dissanayake <sithumsdissanayake@gmail.com>
2020-07-10 21:59:03 +1000

    2

--- frmGame.Designer.vb
+++ frmGame.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class frmGame
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,24 +20,44 @@ Partial Class frmGame
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.pnlGame = New System.Windows.Forms.Panel()
         Me.SuspendLayout()
         '
         'ListBox1
         '
         Me.ListBox1.FormattingEnabled = True
-        Me.ListBox1.Location = New System.Drawing.Point(52, 45)
+        Me.ListBox1.Location = New System.Drawing.Point(825, 26)
         Me.ListBox1.Name = "ListBox1"
-        Me.ListBox1.Size = New System.Drawing.Size(563, 342)
+        Me.ListBox1.Size = New System.Drawing.Size(409, 238)
         Me.ListBox1.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(962, 300)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(137, 63)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'pnlGame
+        '
+        Me.pnlGame.Location = New System.Drawing.Point(12, 26)
+        Me.pnlGame.Name = "pnlGame"
+        Me.pnlGame.Size = New System.Drawing.Size(797, 801)
+        Me.pnlGame.TabIndex = 2
         '
         'frmGame
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(1263, 864)
+        Me.Controls.Add(Me.pnlGame)
+        Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.ListBox1)
         Me.Name = "frmGame"
         Me.Text = "Minesweeper - Game"
@@ -46,4 +66,6 @@ Partial Class frmGame
     End Sub
 
     Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents pnlGame As Panel
 End Class
--- frmGame.vb
+++ frmGame.vb
@@ -1,14 +1,14 @@
 ﻿Public Class frmGame
     'All variable names: playerName, difficulty
-    Dim gameGrid(0, 0) As Integer
     Private Sub frmGame_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Dim playerName As String = "disum"
+
+        Dim gameGrid(0, 0) As Integer
+
         Dim difficulty As Integer = 2
         Dim totalMines As Integer
         Dim gridYLength As Integer
         Dim gridXLength As Integer
-        Dim i, j As Integer
-        Dim mineCoordinates
 
         'textbox for playername
         totalMines = difficultySetMines(difficulty)
@@ -16,12 +16,13 @@
         gridYLength = difficultySelectY(difficulty)
         ReDim gameGrid(gridXLength, gridYLength)
 
-        setGrid(gridXLength, gridYLength)
+        setGrid(gameGrid, gridXLength, gridYLength)
 
-        generateMines(gridXLength, gridYLength, totalMines)
+        generateMines(gameGrid, gridXLength, gridYLength, totalMines)
 
-        populateSquares(gridXLength, gridYLength)
+        populateSquares(gameGrid, gridXLength, gridYLength)
 
+        testButtons(gameGrid)
 
         Dim x, y As Integer
         Dim temp As String
@@ -59,14 +60,14 @@
             Return 99
         End If
     End Function
-    Sub setGrid(gridXLength As Integer, gridYLength As Integer)
+    Function setGrid(gameGrid(,) As Integer, gridXLength As Integer, gridYLength As Integer) As Integer()
         For x = 0 To gridXLength
             For y = 0 To gridYLength
                 gameGrid(x, y) = 0
             Next y
         Next x
-    End Sub
-    Sub generateMines(gridXLength As Integer, gridYLength As Integer, totalMines As Integer)
+    End Function
+    Function generateMines(gameGrid(,) As Integer, gridXLength As Integer, gridYLength As Integer, totalMines As Integer) As Integer()
         Dim checkMines As Integer
         Dim x As Integer
         Dim y As Integer
@@ -74,24 +75,24 @@
         checkMines = 0
 
         While checkMines <> totalMines
-            'Randomize()
             x = RandBetween(0, gridXLength)
             y = RandBetween(0, gridYLength)
 
-            If gameGrid(x, y) <> 999 Then
-                gameGrid(x, y) = 999
+            If gameGrid(x, y) <> 9 Then
+                gameGrid(x, y) = 9
                 checkMines = checkMines + 1
             End If
         End While
 
-    End Sub
+    End Function
     Public Function RandBetween(Low As Integer, High As Integer) As Integer
+        'Randomize()
         RandBetween = Int((High - Low + 1) * Rnd()) + Low
     End Function
 
 
 
-    Sub populateSquares(gridXLength As Integer, gridYLength As Integer)
+    Function populateSquares(gameGrid(,) As Integer, gridXLength As Integer, gridYLength As Integer) As Integer()
         Dim x As Integer
         Dim y As Integer
         Dim yTop As Integer
@@ -107,47 +108,92 @@
 
         For x = 0 To gridXLength
             For y = 0 To gridYLength
-                If gameGrid(x, y) = 999 Then
+                If gameGrid(x, y) = 9 Then
                     yTop = y + 1
                     yBottom = y - 1
                     xLeft = x - 1
                     xRight = x + 1
 
-                    If xLeft <> -1 AndAlso yTop <> yLimit AndAlso gameGrid(xLeft, yTop) <> 999 Then
+                    If xLeft <> -1 AndAlso yTop <> yLimit AndAlso gameGrid(xLeft, yTop) <> 9 Then
                         gameGrid(xLeft, yTop) = gameGrid(xLeft, yTop) + 1
                     End If
 
-                    If yTop <> yLimit AndAlso gameGrid(x, yTop) <> 999 Then
+                    If yTop <> yLimit AndAlso gameGrid(x, yTop) <> 9 Then
                         gameGrid(x, yTop) = gameGrid(x, yTop) + 1
                     End If
 
-                    If xRight <> xLimit AndAlso yTop <> yLimit AndAlso gameGrid(xRight, yTop) <> 999 Then
+                    If xRight <> xLimit AndAlso yTop <> yLimit AndAlso gameGrid(xRight, yTop) <> 9 Then
                         gameGrid(xRight, yTop) = gameGrid(xRight, yTop) + 1
                     End If
 
-                    If xLeft <> -1 AndAlso gameGrid(xLeft, y) <> 999 Then
+                    If xLeft <> -1 AndAlso gameGrid(xLeft, y) <> 9 Then
                         gameGrid(xLeft, y) = gameGrid(xLeft, y) + 1
                     End If
 
-                    If xRight <> xLimit AndAlso gameGrid(xRight, y) <> 999 Then
+                    If xRight <> xLimit AndAlso gameGrid(xRight, y) <> 9 Then
                         gameGrid(xRight, y) = gameGrid(xRight, y) + 1
                     End If
 
-                    If xLeft <> -1 AndAlso yBottom <> -1 AndAlso gameGrid(xLeft, yBottom) <> 999 Then
+                    If xLeft <> -1 AndAlso yBottom <> -1 AndAlso gameGrid(xLeft, yBottom) <> 9 Then
                         gameGrid(xLeft, yBottom) = gameGrid(xLeft, yBottom) + 1
                     End If
 
-                    If yBottom <> -1 AndAlso gameGrid(x, yBottom) <> 999 Then
+                    If yBottom <> -1 AndAlso gameGrid(x, yBottom) <> 9 Then
                         gameGrid(x, yBottom) = gameGrid(x, yBottom) + 1
                     End If
 
-                    If xRight <> xLimit AndAlso yBottom <> -1 AndAlso gameGrid(xRight, yBottom) <> 999 Then
+                    If xRight <> xLimit AndAlso yBottom <> -1 AndAlso gameGrid(xRight, yBottom) <> 9 Then
                         gameGrid(xRight, yBottom) = gameGrid(xRight, yBottom) + 1
                     End If
 
                 End If
             Next y
         Next x
+    End Function
+
+    Sub testbuttons(gamegrid(,) As Integer)
+        Dim x, y As Integer
+
+        For y = 0 To 15
+            For x = 0 To 15
+                Dim test As New Button
+                test.Name = gamegrid(x, y)
+
+                test.Height = 50
+                test.Width = 50
+
+                test.Left = test.Left + (49 * x)
+                test.Top = test.Top + (49 * y)
+
+                pnlGame.Controls.Add(test)
+                AddHandler test.Click, AddressOf Btn_Click
+
+
+                Dim test2 As New Label
+                test2.Text = gamegrid(x, y)
+
+                test2.Height = 50
+                test2.Width = 50
+
+                test2.Left = test2.Left + (49 * x)
+                test2.Top = test2.Top + (49 * y)
+
+                pnlGame.Controls.Add(test2)
+
+            Next x
+        Next y
     End Sub
 
+    Private Sub Btn_Click(sender As Object, e As EventArgs)
+        Dim buttonValue As Integer
+
+        buttonValue = sender.name
+
+        sender.visible = False
+
+        If buttonValue = 9 Then
+            MsgBox("gameover")
+        End If
+
+    End Sub
 End Class
